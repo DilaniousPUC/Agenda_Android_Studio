5ca26ec25e8f2901d538075fb13ffbf3d04dfd88
dilan <dilan.colangelo@gmail.com>
2024-05-21 21:33:38 -0300

    Etapa - 4

--- app/src/main/java/com/example/exemplofragmento/Fragmento1.java
+++ app/src/main/java/com/example/exemplofragmento/Fragmento1.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
 
 // Repositório com exemplo de Fragmentos estáticos
 
