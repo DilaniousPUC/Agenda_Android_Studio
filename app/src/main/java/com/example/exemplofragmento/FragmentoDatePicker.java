037f4b4cb8a867ca4585850676e576bb9fe899c7
dilan <dilan.colangelo@gmail.com>
2024-05-27 23:43:11 -0300

    Etapa - 4

--- app/src/main/java/com/example/exemplofragmento/FragmentoDatePicker.java
+++ app/src/main/java/com/example/exemplofragmento/FragmentoDatePicker.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
--- app/src/main/java/com/example/exemplofragmento/FragmentoOtherDatePicker.java
+++ app/src/main/java/com/example/exemplofragmento/FragmentoOtherDatePicker.java
@@ -7,7 +7,6 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
